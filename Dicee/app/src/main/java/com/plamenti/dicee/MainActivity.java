ea873c90960c62493b0e7a75c481bc141ccb29e4
plamenti <plamenti_@abv.bg>
2018-05-07 15:12:33 +0300

    Add button and dices vars

--- Dicee/app/src/main/java/com/plamenti/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/plamenti/dicee/MainActivity.java
@@ -1,7 +1,10 @@
 package com.plamenti.dicee;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.rollbutton);
+        ImageView leftDice = (ImageView) findViewById(R.id.image_leftDice);
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
     }
 }
